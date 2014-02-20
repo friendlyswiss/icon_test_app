aac8c63ab0168c534d7f7d7fe621512288f61ec4
Curt Arledge <cparledge@gmail.com>
2014-02-19 21:48:18 -0500

    Fixed to show accuracy correctly for All People

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -45,29 +45,33 @@ class Trial < ActiveRecord::Base
 			
 			if style == 'both' && color == 'both'
 				labels = ["<span class=\'xaxis solid-black\'>Solid Black</span>" ,"<span class=\'xaxis hollow-black\'>Hollow Black</span>","<span class=\'xaxis solid-white\'>Solid White</span>","<span class=\'xaxis hollow-white\'>Hollow White</span>"]
+				denom = self.count * 0.0025
 				groupdata = [
-					((self.where("style = ? AND color = ? AND task_success = ?", 'solid', 'white_bg', true).count.to_f / 0.06).round(1)),
-					((self.where("style = ? AND color = ? AND task_success = ?", 'hollow', 'white_bg', true).count.to_f / 0.06).round(1)),
-					((self.where("style = ? AND color = ? AND task_success = ?", 'solid', 'black_bg', true).count.to_f / 0.06).round(1)),
-					((self.where("style = ? AND color = ? AND task_success = ?", 'hollow', 'black_bg', true).count.to_f / 0.06).round(1))
+					((self.where("style = ? AND color = ? AND task_success = ?", 'solid', 'white_bg', true).count.to_f / denom).round(1)),
+					((self.where("style = ? AND color = ? AND task_success = ?", 'hollow', 'white_bg', true).count.to_f / denom).round(1)),
+					((self.where("style = ? AND color = ? AND task_success = ?", 'solid', 'black_bg', true).count.to_f / denom).round(1)),
+					((self.where("style = ? AND color = ? AND task_success = ?", 'hollow', 'black_bg', true).count.to_f / denom).round(1))
 				]
 
 			elsif style == 'either' && color == 'either'
 				labels = ["All"]
-				groupdata = [(self.where(task_success: true).count.to_f / 0.24).round(1)]
+				denom = self.count * 0.01
+				groupdata = [(self.where(task_success: true).count.to_f / denom).round(1)]
 			
 			elsif style == 'both' && color == 'either'
 				labels = ["<span class=\'xaxis solid text-center\'>Solid</span>", "<span class=\'xaxis hollow text-center\'>Hollow</span"]				
+				denom = self.count*0.005
 				groupdata = [
-					((self.where("style = ? AND task_success = ?", 'solid', true).count.to_f / 0.12).round(1)),
-					((self.where("style = ? AND task_success = ?", 'hollow', true).count.to_f / 0.12).round(1))
+					((self.where("style = ? AND task_success = ?", 'solid', true).count.to_f / denom).round(1)),
+					((self.where("style = ? AND task_success = ?", 'hollow', true).count.to_f / denom).round(1))
 				]
 
 			elsif style == 'either' && color == 'both'
 				labels = ["<span class=\'xaxis black\'>Black on White</span>", "<span class=\'xaxis white\'>White on Black</span>"]
+				denom = self.count*0.005
 				groupdata = [
-					((self.where("color = ? AND task_success = ?", 'white_bg', true).count.to_f / 0.12).round(1)),
-					((self.where("color = ? AND task_success = ?", 'black_bg', true).count.to_f / 0.12).round(1))
+					((self.where("color = ? AND task_success = ?", 'white_bg', true).count.to_f / denom).round(1)),
+					((self.where("color = ? AND task_success = ?", 'black_bg', true).count.to_f / denom).round(1))
 				]
 			end
 		end
